6812956478e3d92a91b4bc7776701b38de90989b
i-dipanshu-tripathi <i.dipanshu.tripathi@gmail.com>
2025-06-14 11:41:07 +0530

    Day 2 LowerBound

---
+++ BINARY_SEARCH/2.LOWER_BOUND/2.2LOWER_BOUND_ITERATIVE2.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int lowerBound(vector<int> &nums, int x)
+    {
+        // lower bound element is smallest element greater than or equal to the given no arr[i]>=x
+        // SC -> O(1)
+        // TC -> O(LOG(N))
+
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] >= x)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+};
+
+int main()
+{
+    cout << "Enter the size of the array " << endl;
+    int size;
+    cin >> size;
+
+    cout << "Take the input for the array " << endl;
+    vector<int> nums(size);
+
+    for (int i = 0; i < size; i++)
+        cin >> nums[i];
+
+    cout << "Enter the target element" << endl;
+    int target;
+    cin >> target;
+
+    Solution obj;
+
+    cout << "Lower bound of " << target << " is at index " << obj.lowerBound(nums, target) << endl;
+}
